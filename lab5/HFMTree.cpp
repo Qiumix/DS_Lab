354b5c21ca0be3b9ee15fe423fa39c329e81830e
Qiumix <j487428862@gmail.com>
2025-12-03 20:45:34 +0800

    [WIP] lab5

--- lab5/HFMTree.cpp
+++ lab5/HFMTree.cpp
@@ -1,6 +1,7 @@
 #include <bitset>
 #include <iostream>
 #include <set>
+#include <stack>
 #include <stddef.h>
 #include <vector>
 
@@ -28,21 +29,23 @@ public:
 template <typename T> class HFMTree {
 private:
   set<int> build_helper;
+  int root;
+
   vector<Node<T> /**/> nodes_; // 万恶的c++03把泛型识别成位移
   pair<int, int> find_smallest() {
     int s1 = -1, s2 = -1;
-    int weight = 0x3f3f3f3f / 2;
+    int weight = 0x3f3f3f3f;
     for (int i = 0; i < nodes_.size(); i++) {
       Node<T> node = nodes_[i];
       if (build_helper.find(i) != build_helper.end()) {
         continue;
       }
-      if (node.weight < weight) {
+      if (node.weight_ < weight) {
         weight = node.weight_;
         s1 = i;
       }
     }
-    weight = 0x3f3f3f3f / 2;
+    weight = 0x3f3f3f3f;
     for (int i = 0; i < nodes_.size(); i++) {
       Node<T> node = nodes_[i];
       if (build_helper.find(i) != build_helper.end() || i == s1) {
@@ -59,7 +62,7 @@ private:
   }
 
 public:
-  HFMTree(int size = 10) { nodes_.resize(size * 2); }
+  HFMTree(int size = 10) : root(-1) { nodes_.resize(size * 2); }
   void insert(T v, int weight) { nodes_.push_back(Node<T>(v, weight)); }
   void build() {
     for (int i = 0; i < nodes_.size(); i++) {
@@ -70,6 +73,7 @@ public:
       nodes_[indexes.second].par_index_ = nodes_.size();
       nodes_.push_back(Node<T>(T(), weight, indexes.first, indexes.second));
     }
+    root = nodes_.size() - 1;
   }
 
   // 返回对应index所表示节点的编码的string
@@ -77,8 +81,24 @@ public:
     string path(nodes_.size() / 2);
     char one = '1';
     char zero = '0';
-
-    path.reserve();
+    stack<int> indexes;
+    while (index != -1) {
+      indexes.push(index);
+      index = nodes_[index].par_index_;
+    }
+    index = root;
+    while (true) {
+      int i = indexes.top();
+      indexes.pop();
+      if (nodes_[index].left_index_ == i) {
+        path.push_back(zero);
+      } else if (nodes_[index].right_index_ == i) {
+        path.push_back(one);
+      } else {
+        break;
+      }
+      index = i;
+    }
     return path;
   }
 };
